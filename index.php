778caf8669da7b393485e9bf1d670edeb0def06e
far-ago <samsuka2288@gmail.com>
2021-07-05 14:31:18 +0300

    edit readme

--- index.php
+++ index.php
@@ -3,11 +3,5 @@ use MyApp\Weather;
 
 require_once __DIR__."/vendor/autoload.php";
 
-/**
- * Class Req
- * @package src
- */
-
-
 $data = new Weather('e114911e93b54a39a92134016210207');
 $data->getWeatherByCity();
